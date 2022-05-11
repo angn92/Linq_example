85ab4be1fd65091e334f8e28836104f886b92df6
DESKTOP-I1C8DIN\gnute <gnutekandrzej@gmail.com>
2022-05-11 22:57:34 +0200

    Config

---
+++ Domain/ContactType.cs
@@ -0,0 +1,10 @@
+﻿namespace Linq_example.Domain
+{
+    public class ContactType
+    {
+        public int ContectTypeID { get; set; }
+        public string Name { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
